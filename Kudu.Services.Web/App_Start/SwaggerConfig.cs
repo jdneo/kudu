85d5d53932c244a2353b29effae457db551bbf31
Eric Jizba <erijiz@microsoft.com>
2018-02-26 07:49:47 -0800

    Allow 204 for putItem calls

--- Kudu.Services.Web/App_Start/SwaggerConfig.cs
+++ Kudu.Services.Web/App_Start/SwaggerConfig.cs
@@ -48,7 +48,7 @@ public class AcceptedResponseFilter : IOperationFilter
                 operation.responses.Remove(((int)HttpStatusCode.NoContent).ToString());
                 operation.responses.Add(((int)HttpStatusCode.OK).ToString(), response);
             }
-        } else if (operation.operationId == "Function_Delete")  // controller and action name
+        } else if (operation.operationId == "Function_Delete" || operation.operationId.EndsWith("_PutItem"))  // controller and action name
         {
             var response = operation.responses.First().Value;
             operation.responses.Add(((int)HttpStatusCode.NoContent).ToString(), response);
